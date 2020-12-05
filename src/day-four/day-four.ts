9740ff2c76f1b95111040c97032ec5f79fa7ceb7
Devyn Cyphers <devyncyphers@gmail.com>
2020-12-05 14:40:45 -0700

    format

--- src/day-four/day-four.ts
+++ src/day-four/day-four.ts
@@ -1,40 +1,50 @@
+const ValidKeys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+const ValidColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
+
+export const validators: {
+  [k: string]: (k: string, v: { [k: string]: string }) => boolean;
+} = {
+  byr: (k, v) => +v[k] >= 1920 && +v[k] <= 2002,
+  iyr: (k, v) => +v[k] >= 2010 && +v[k] <= 2020,
+  eyr: (k, v) => +v[k] >= 2020 && +v[k] <= 2030,
+  hgt: (k, v) => {
+    if (!v[k]) return false;
+    const c = v[k].substring(v[k].length - 2);
+    const n = +v[k].substring(0, v[k].length - 2);
+    if (c === 'cm') return n >= 150 && n <= 193;
+    if (c === 'in') return n >= 59 && n <= 76;
+    return false;
+  },
+  hcl: (k, v) => v[k] && !!v[k].match(/#[0-9a-f]{6}/g),
+  ecl: (k, v) => ValidColors.some((c) => v[k] === c),
+  pid: (k, v) => +v[k] && +v[k].length === 9,
+};
 
 export const parsePassports = (data: string) => {
-    const ValidKeys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
-
-    return data
-        .split('\n\n')
-        .filter(v => v)
-        .map(v => JSON.parse(`{${v.replace(/[\w#]+/g, '"$&"').trimEnd().replace(/[\n\s]/g, ',')}}`))
-        .filter(v => ValidKeys.every(k => v[k]))
-        .length;
-}
-
-const ValidColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
-export const validators: {[k:string]: (k:string,v:{[k:string]:string})=>boolean} = {
-    byr: (k,v) => +v[k] >=1920 && +v[k] <= 2002,
-    iyr: (k,v) => +v[k] >=2010 && +v[k] <= 2020,
-    eyr: (k,v) => +v[k] >=2020 && +v[k] <= 2030,
-    hgt: (k,v) => {
-        if (!v[k]) return false;
-        const c = v[k].substring(v[k].length-2);
-        const n = +v[k].substring(0, v[k].length-2);
-        if(c === 'cm') return n >=150 && n <=193;
-        if(c === 'in') return n >=59 && n <=76;
-        return false;
-    },
-    hcl: (k,v) => v[k] && !!v[k].match(/#[0-9a-f]{6}/g),
-    ecl: (k,v) => ValidColors.some(c => v[k] === c),
-    pid: (k,v) => +v[k] && +v[k].length === 9
-}
-
+  return data
+    .split('\n\n')
+    .filter((v) => v)
+    .map((v) =>
+      JSON.parse(
+        `{${v
+          .replace(/[\w#]+/g, '"$&"')
+          .trimEnd()
+          .replace(/[\n\s]/g, ',')}}`
+      )
+    )
+    .filter((v) => ValidKeys.every((k) => v[k])).length;
+};
 export const parsePassportsStrict = (data: string) => {
-    const ValidKeys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
-
-    return data
-        .split('\n\n')
-        .filter(v => v)
-        .map(v => JSON.parse(`{${v.replace(/[\w#]+/g, '"$&"').trimEnd().replace(/[\n\s]/g, ',')}}`))
-        .filter(v => ValidKeys.every(k => validators[k](k, v)))
-        .length;
-}
+  return data
+    .split('\n\n')
+    .filter((v) => v)
+    .map((v) =>
+      JSON.parse(
+        `{${v
+          .replace(/[\w#]+/g, '"$&"')
+          .trimEnd()
+          .replace(/[\n\s]/g, ',')}}`
+      )
+    )
+    .filter((v) => ValidKeys.every((k) => validators[k](k, v))).length;
+};
--- src/day-three/day-three.ts
+++ src/day-three/day-three.ts
@@ -1,32 +1,37 @@
 export const calcColissions = (data: string) => {
-    let index = -3;
-    return data
-        .split('\n')
-        .map(v => v.trim())
-        .reduce<number>((prev, curr) => {
-            index+=3;
-            return curr[(index%curr.length)] === '#' ? prev + 1 : prev;
-        }, 0);
+  let index = -3;
+  return data
+    .split('\n')
+    .map((v) => v.trim())
+    .reduce<number>((prev, curr) => {
+      index += 3;
+      return curr[index % curr.length] === '#' ? prev + 1 : prev;
+    }, 0);
 };
 
 export const calcColissionsMultiple = (data: string) => {
-    const calcPath = ([rise, run]: number[]) => {
-        let index = -rise;
+  const calcPath = ([rise, run]: number[]) => {
+    let index = -rise;
 
-        return data
-            .split('\n')
-            .map(v => v.trim())
-            .filter((v, i) => {
-                return run > 1 ? !((i)%run) : true
-            })
-            .reduce<number>((prev, curr) => {
-                index+=rise;
-                return curr[(index%curr.length)] === '#' ? prev + 1 : prev;
-            }, 0);
-    }
+    return data
+      .split('\n')
+      .map((v) => v.trim())
+      .filter((v, i) => {
+        return run > 1 ? !(i % run) : true;
+      })
+      .reduce<number>((prev, curr) => {
+        index += rise;
+        return curr[index % curr.length] === '#' ? prev + 1 : prev;
+      }, 0);
+  };
 
-    return [[1,1],[3,1],[5,1],[7,1],[1,2]].reduce<number>((prev, curr) => {
-        return prev *= calcPath(curr);
-    }, 1);
-
-}
+  return [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+  ].reduce<number>((prev, curr) => {
+    return (prev *= calcPath(curr));
+  }, 1);
+};
